fe3b52930f1bb0b464fbb3f323f69c0de919c5ef
CleverUserName <wellhimrhell@gmail.com>
2026-07-10 02:27:03 +1000

    finished roto 2

--- Game_dev_3/AGENTS.md
+++ Game_dev_3/AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: Game_dev_3
 - Unity version: Unity 6000.3.6f1
 - Active game object:
-  - Name: Spawn Manager
+  - Name: ChoiceChooser
   - Tag: Untagged
-  - Layer: Default
+  - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Game_dev_3/Assets/Prototype 2/source/Choices.cs
+++ Game_dev_3/Assets/Prototype 2/source/Choices.cs
@@ -15,24 +15,119 @@ public class Choices : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-   public void ClickOnMe()
+    public void ClickOnMe()
     {
         Scene currentScene = SceneManager.GetActiveScene();
+        Debug.Log($"Clicked. Scene: {currentScene.name}, Choice: {myChoice}");
+
 
         if (currentScene.name == "Dialouge_Begins" && myChoice == CHOICE.CHOICE1)
         {
-            SceneManager.LoadScene("branch_1_1");
+            SceneManager.LoadScene("1_1");
+        }
+        if (currentScene.name == "Dialouge_Begins" && myChoice == CHOICE.CHOICE2)
+        {
+            SceneManager.LoadScene("1_2");
         }
 
+        if (currentScene.name == "1_1" && myChoice == CHOICE.CHOICE1)
+        {
+            SceneManager.LoadScene("1_1_1");
+        }
+        if (currentScene.name == "1_1" && myChoice == CHOICE.CHOICE2)
+        {
+            SceneManager.LoadScene("1_1_2");
+        }
+
+        if (currentScene.name == "1_1_1" && myChoice == CHOICE.CHOICE1)
+        {
+            SceneManager.LoadScene("1_com");
+        }
+        if (currentScene.name == "1_1_1" && myChoice == CHOICE.CHOICE2)
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+
+        if (currentScene.name == "1_1_2" && myChoice == CHOICE.CHOICE1)
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+        if (currentScene.name == "1_1_2" && myChoice == CHOICE.CHOICE2)
+        {
+            SceneManager.LoadScene("1_com");
+        }
+
+        if (currentScene.name == "1_2" && myChoice == CHOICE.CHOICE1)
+        {
+            SceneManager.LoadScene("1_2_1");
+        }
+        if (currentScene.name == "1_2" && myChoice == CHOICE.CHOICE2)
+        {
+            SceneManager.LoadScene("1_2_2");
+        }
+
+        if (currentScene.name == "1_2_1" && myChoice == CHOICE.CHOICE1)
+        {
+            SceneManager.LoadScene("1_com");
+        }
+        if (currentScene.name == "1_2_1" && myChoice == CHOICE.CHOICE2)
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+
+        if (currentScene.name == "1_2_2" && myChoice == CHOICE.CHOICE1)
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+        if (currentScene.name == "1_2_2" && myChoice == CHOICE.CHOICE2)
+        {
+            SceneManager.LoadScene("1_com");
+        }
+
+        if (currentScene.name == "1_com" && myChoice == CHOICE.CHOICE1)
+        {
+            SceneManager.LoadScene("1_com_1");
+        }
+        if (currentScene.name == "1_com" && myChoice == CHOICE.CHOICE2)
+        {
+            SceneManager.LoadScene("1_com_2");
+        }
+
+        if (currentScene.name == "1_com_1" && myChoice == CHOICE.CHOICE1)
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+        if (currentScene.name == "1_com_1" && myChoice == CHOICE.CHOICE2)
+        {
+            SceneManager.LoadScene("1_com_com");
+        }
+
+        if (currentScene.name == "1_com_2" && myChoice == CHOICE.CHOICE1)
+        {
+            SceneManager.LoadScene("1_com_com");
+        }
+        if (currentScene.name == "1_com_2" && myChoice == CHOICE.CHOICE2)
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+
+        if (currentScene.name == "1_com_com" && myChoice == CHOICE.CHOICE1)
+        {
+            SceneManager.LoadScene("good_end");
+        }
+        if (currentScene.name == "1_com_com" && myChoice == CHOICE.CHOICE2)
+        {
+            SceneManager.LoadScene("bad_end");
+        }
     }
 
     public void Consequences()
@@ -43,15 +138,104 @@ public class Choices : MonoBehaviour
         // Check by scene name
         if (currentScene.name == "Dialouge_Begins" && gameObject.name == "Choice 1")
         {
-            SceneManager.LoadScene("branch_1_1");
+            SceneManager.LoadScene("1_1");
         }
-
-        // Check by scene name
         if (currentScene.name == "Dialouge_Begins" && gameObject.name == "Choice 2")
         {
-            SceneManager.LoadScene("branch_2_1");
+            SceneManager.LoadScene("1_2");
         }
-    }
 
+        if (currentScene.name == "1_1" && gameObject.name == "Choice 1")
+        {
+            SceneManager.LoadScene("1_1_1");
+        }
+        if (currentScene.name == "1_1" && gameObject.name == "Choice 2")
+        {
+            SceneManager.LoadScene("1_1_2");
+        }
+
+        if (currentScene.name == "1_1_1" && gameObject.name == "Choice 1")
+        {
+            SceneManager.LoadScene("1_com");
+        }
+        if (currentScene.name == "1_1_1" && gameObject.name == "Choice 2")
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+
+        if (currentScene.name == "1_1_2" && gameObject.name == "Choice 1")
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+        if (currentScene.name == "1_1_2" && gameObject.name == "Choice 2")
+        {
+            SceneManager.LoadScene("1_com");
+        }
+
+        if (currentScene.name == "1_2" && gameObject.name == "Choice 1")
+        {
+            SceneManager.LoadScene("1_2_1");
+        }
+        if (currentScene.name == "1_2" && gameObject.name == "Choice 2")
+        {
+            SceneManager.LoadScene("1_2_2");
+        }
+
+        if (currentScene.name == "1_2_1" && gameObject.name == "Choice 1")
+        {
+            SceneManager.LoadScene("1_com");
+        }
+        if (currentScene.name == "1_2_1" && gameObject.name == "Choice 2")
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+
+        if (currentScene.name == "1_2_2" && gameObject.name == "Choice 1")
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+        if (currentScene.name == "1_2_2" && gameObject.name == "Choice 2")
+        {
+            SceneManager.LoadScene("1_com");
+        }
+
+        if (currentScene.name == "1_com" && gameObject.name == "Choice 1")
+        {
+            SceneManager.LoadScene("1_com_1");
+        }
+        if (currentScene.name == "1_com" && gameObject.name == "Choice 2")
+        {
+            SceneManager.LoadScene("1_com_2");
+        }
+
+        if (currentScene.name == "1_com_1" && gameObject.name == "Choice 1")
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+        if (currentScene.name == "1_com_1" && gameObject.name == "Choice 2")
+        {
+            SceneManager.LoadScene("1_com_com");
+        }
+
+        if (currentScene.name == "1_com_2" && gameObject.name == "Choice 1")
+        {
+            SceneManager.LoadScene("1_com_com");
+        }
+        if (currentScene.name == "1_com_2" && gameObject.name == "Choice 2")
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+
+        if (currentScene.name == "1_com_com" && gameObject.name == "Choice 1")
+        {
+            SceneManager.LoadScene("good_end");
+        }
+        if (currentScene.name == "1_com_com" && gameObject.name == "Choice 2")
+        {
+            SceneManager.LoadScene("bad_end");
+        }
+
+
+    }
 
 }
